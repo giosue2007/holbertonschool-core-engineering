0092371083383c44e340f168705521797f457b4e
giosue2007 <toxicgiosue@gmail.com>
2026-09-01 11:10:38 +0200

    add perm and c

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-import random
-
-number = random.randint(-10, 10)
-
+number = __import__('random').randint(-10, 10)
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
