40d07c7968ebc40acc21dde719b23173f4b3df18
giosue2007 <toxicgiosue@gmail.com>
2026-09-08 14:00:57 +0200

    add perm and c

--- python_oop/classes_and_object_model/1-square.py
+++ python_oop/classes_and_object_model/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+"""Square class module."""
 
 
 class Square:
+    """Square class."""
 
     def __init__(self, size):
+        """Initialize square with size."""
         self.__size = size
