79ff5db939b6b4265bf6f641df1fe05d08002ed0
giosue2007 <toxicgiosue@gmail.com>
2026-09-10 10:19:54 +0200

    add perm and c

---
+++ python_oop/inheritance/base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""BaseGeometry class module."""
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+
+    def area(self):
+        """Raise Exception for unimplemented area method."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate value as a positive integer."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
