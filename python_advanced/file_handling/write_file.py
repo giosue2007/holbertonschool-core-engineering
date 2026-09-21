54344fb07b45c6d1cb201b7ac767ae8558719d68
giosue2007 <toxicgiosue@gmail.com>
2026-09-21 15:23:06 +0200

    add perm and c

---
+++ python_advanced/file_handling/write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module containing the write_file function.
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns character count."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
