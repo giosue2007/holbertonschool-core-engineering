4ce12fa25c0eccc0a5560aa3b5530887212ec8b1
giosue2007 <toxicgiosue@gmail.com>
2026-09-21 09:35:40 +0200

    add perm and c

---
+++ python_advanced/file_handling/read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module containing the read_file function.
+"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints its content to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
